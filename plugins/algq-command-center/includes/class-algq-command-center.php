9e41ecb4a8740d0a4b0ae612f817fcd9c10951e3
ohi-stack <onegodianone@gmail.com>
2026-07-31 07:13:17 -0400

    Register Command Center health and reporting services

--- plugins/algq-command-center/includes/class-algq-command-center.php
+++ plugins/algq-command-center/includes/class-algq-command-center.php
@@ -5,14 +5,21 @@
  * @package Algonquian_Command_Center
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
-class ALGQ_Command_Center {
-	public function run() {
-		( new ALGQ_Command_Center_Assets() )->register();
-		( new ALGQ_Command_Center_Shortcodes() )->register();
-		( new ALGQ_Command_Center_Admin() )->register();
-	}
+final class ALGQ_Command_Center {
+    public function run(): void {
+        ( new ALGQ_Command_Center_Assets() )->register();
+        ( new ALGQ_Command_Center_Shortcodes() )->register();
+        ( new ALGQ_Command_Center_Admin() )->register();
+        ( new ALGQ_Command_Center_Report_Controller() )->register();
+
+        add_filter(
+            'algq_command_center_metrics',
+            static function ( array $metrics ): array {
+                $metrics['system_health'] = ALGQ_Command_Center_Health_Monitor::summary();
+                return $metrics;
+            }
+        );
+    }
 }
